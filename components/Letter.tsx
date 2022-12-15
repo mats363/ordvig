81a9a803537556921c828203000d882d8e711002
Mats Jönsson <hello@matsjonsson.work>
2022-12-15 10:50:26 +0100

    fix

--- components/Letter.tsx
+++ components/Letter.tsx
@@ -9,20 +9,30 @@ type letterProps = {
 type CtxModel = {
   board: [][];
   answer: string;
+  currentAttempt: any;
 };
 
 export const Letter: React.FC<letterProps> = ({ position, attempt }) => {
-  const { board, answer } = useContext<CtxModel>(AppContext);
-  const letter = board[attempt][position];
+  const { board, answer, currentAttempt } = useContext<CtxModel>(AppContext);
+  const letter: string = board[attempt][position];
 
   const correct: boolean = answer[position] === letter;
   const almost: boolean =
-    !correct && letter !== "" && answer.toString().includes("a");
+    !correct && letter !== "" && answer.toString().includes(letter);
 
-  const letterState = correct ? "Correct" : almost ? "Almost" : "Error";
+  // const letterState =
+  //   currentAttempt.attempt > attempt && correct
+  //     ? "bg-gray-500"
+  //     : almost
+  //     ? "bg-yellow"
+  //     : "bg-grey";
+  const letterState = "";
+  console.log(correct);
 
   return (
-    <div className="h-16 w-16 text-4xl flex border-2 border-gray-300 p-1 m-1 box-border justify-center items-center">
+    <div
+      className={`h-16 w-16 text-4xl flex border-2 border-gray-300 p-1 m-1 box-border justify-center items-center ${letterState}`}
+    >
       {letter}
     </div>
   );
